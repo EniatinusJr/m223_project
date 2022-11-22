96566231700d452cdb79551d92b63ae2f11e7553
Maurin Schucan <maurin@schucan.ch>
2022-11-22 17:03:06 +0000

    added endpoint to register

--- src/main/java/ch/zli/m223/controller/AuthController.java
+++ src/main/java/ch/zli/m223/controller/AuthController.java
@@ -1,6 +1,7 @@
 package ch.zli.m223.controller;
-/*
+
 import javax.inject.Inject;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -9,24 +10,34 @@ import javax.ws.rs.core.MediaType;
 
 import org.eclipse.microprofile.openapi.annotations.Operation;
 
+import ch.zli.m223.model.User;
 import ch.zli.m223.service.AuthService;
 
-@Path("/login")
+@Path("/auth")
 public class AuthController {
     
     @Inject
     AuthService authService;
-    
+    /*
     @POST
-    @Path("/{username}/{password}")
+    @Path("/login/{email}/{password}")
     @Produces(MediaType.APPLICATION_JSON)
-    @Operation(summary = "Index user.", description = "Returns a JWT if login successful.")
-    public String authlogin(@PathParam("username") String username, @PathParam("password") String password) {
+    @Operation(summary = "Index user", description = "Returns a JWT if login successful.")
+    public String authlogin(@PathParam("email") String username, @PathParam("password") String password) {
         if(authService.userExists(username, password) == true){
             return authService.generateToken();
         }else{
             return null;
         }
+    }*/
+
+    @POST
+    @Path("/register")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Operation(summary = "Creates a new User.", description = "Creates a new User and returns the newly added booking.")
+    public User create(User user) {
+       return authService.createUser(user);
     }
 
-}*/
+}
--- src/main/java/ch/zli/m223/service/AuthService.java
+++ src/main/java/ch/zli/m223/service/AuthService.java
@@ -1,11 +1,13 @@
 package ch.zli.m223.service;
-/*
+
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
 
 import org.eclipse.microprofile.jwt.Claims;
 
@@ -15,7 +17,7 @@ import io.smallrye.jwt.build.Jwt;
 @ApplicationScoped
 public class AuthService {
     @Inject
-    private UserService userService;
+    private EntityManager entityManager;
     
     public String generateToken() {
         String token =
@@ -26,15 +28,23 @@ public class AuthService {
            .sign();
         return token;
     }
-
-    public boolean userExists(String username, String password) {
+/*
+    @Transactional
+    public boolean userExists(String email, String password) {
         boolean doesExist = false;
-        List<User> users = userService.findAll();
+        var entities = entityManager.createQuery("FROM User", User.class);
+        List<User> users = entities.getResultList();
         for(User user : users) {
-            if(user.getUsername().equals(username) && user.getPassword().equals(password)) {
+            if(user.getEmail().equals(email) && user.getPassword().equals(password)) {
                 doesExist = true;
             }
         }
         return doesExist;
+    }*/
+
+    @Transactional
+    public User createUser(User user) {
+        entityManager.persist(user);
+        return user;
     }
-}*/
+}
